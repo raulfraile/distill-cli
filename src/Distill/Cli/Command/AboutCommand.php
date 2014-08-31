a3b4adc8cf2b1b2c111848ad718b1cde49b8ccc2
Raul Fraile <raulfraile@gmail.com>
2014-08-31 18:04:13 +0200

    CS fixes

--- src/Distill/Cli/Command/AboutCommand.php
+++ src/Distill/Cli/Command/AboutCommand.php
@@ -3,10 +3,7 @@
 namespace Distill\Cli\Command;
 
 use Distill\Distill;
-use Distill\File;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\ProgressBar;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
--- src/Distill/Cli/Command/ExtractCommand.php
+++ src/Distill/Cli/Command/ExtractCommand.php
@@ -6,7 +6,6 @@ use Distill\Distill;
 use Distill\File;
 use Distill\Format\FormatGuesser;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
